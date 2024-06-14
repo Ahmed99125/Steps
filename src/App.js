0b52a9fb3425e37f532491b2a2df6bb632a13850
Ahmed99125 <at99125@gmail.com>
2024-06-14 03:02:07 +0300

    uploading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+  const [step, setStep] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handlePrev = () => {
+    if (step > 0) setStep(step - 1);
+  };
+
+  const hadnleNext = () => {
+    if (step < 2) setStep(step + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 0 ? "active" : ""}>1</div>
+            <div className={step >= 1 ? "active" : ""}>2</div>
+            <div className={step >= 2 ? "active" : ""}>3</div>
+          </div>
+          <div className="message">
+            Step {step + 1}: {messages[step]}
+          </div>
+          <div className="buttons">
+            <button
+              style={{ color: "#fff", backgroundColor: "#7950f2" }}
+              onClick={handlePrev}
+            >
+              Previous
+            </button>
+            <button
+              style={{ color: "#fff", backgroundColor: "#7950f2" }}
+              onClick={hadnleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
